0b0e5e7c7e28b9ba3c172f7d165d09fe530ebb3a
Nathan Sheehy <nimolimolo@gmail.com>
2021-07-17 17:37:57 -0400

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
     #attr_accessor :adress
     skip_before_action :verify_authenticity_token, :only => :create
+    skip_before_action :verify_authenticity_token, :only => :destroy
 end
